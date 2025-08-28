6abc1b676e539e3da63b8ebef30bec49507f575c
Vit0rSilva <vitor.work21@gmail.com>
2025-08-28 16:59:34 -0300

    Carrosel de pratos estilizando

--- swiper.js
+++ swiper.js
@@ -17,3 +17,44 @@ const swiper = new Swiper(
       
     }
 );
+
+const swiperCarrosel = new Swiper('.swiper-carrosel-pratos', {
+  
+  slidesPerView: 4,
+  spaceBetween: 30,
+  grid:{
+    rows:2,
+    fill:"row",
+  },
+
+  // Responsividade
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2,
+      },
+    },
+    768: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2,
+      },
+    },
+    1024: {
+      slidesPerView: 4,
+      grid: {
+        rows: 2,
+      },
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
