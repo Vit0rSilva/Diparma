96749fa167b1f60edfed3cfb1ab019c464cd5443
Vit0rSilva <vitor.work21@gmail.com>
2025-08-29 16:43:42 -0300

    Carrosel de prato

--- swiper.js
+++ swiper.js
@@ -2,7 +2,7 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 
 // init Swiper:
 const swiper = new Swiper(
-    '.swiper', {
+    '.swiper-carrosel', {
       slidesPerView: 2,       // Quantos slides aparecem por vez
     spaceBetween: 30,       // Espaço entre os slides
     initialSlide: 4,        // Começa no último slide (base 0, então 4 é o 5º slide)
@@ -19,8 +19,7 @@ const swiper = new Swiper(
 );
 
 const swiperCarrosel = new Swiper('.swiper-carrosel-pratos', {
-  
-  slidesPerView: 4,
+  slidesPerView: 1.4,
   spaceBetween: 30,
   grid:{
     rows:2,
@@ -29,20 +28,26 @@ const swiperCarrosel = new Swiper('.swiper-carrosel-pratos', {
 
   // Responsividade
   breakpoints: {
-    640: {
-      slidesPerView: 2,
+    540: {
+      slidesPerView: 2.2,
       grid: {
         rows: 2,
       },
     },
-    768: {
-      slidesPerView: 3,
+    780: {
+      slidesPerView: 2.6,
       grid: {
         rows: 2,
       },
     },
-    1024: {
-      slidesPerView: 4,
+    998: {
+      slidesPerView: 3.5,
+      grid: {
+        rows: 2,
+      },
+    },
+    1194: {
+      slidesPerView: 4.6,
       grid: {
         rows: 2,
       },
