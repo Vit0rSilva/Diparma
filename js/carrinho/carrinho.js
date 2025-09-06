77df7bd320db7420a0962fad4af89d03b68f8306
Vitor Silva <188924537+Vit0rSilva@users.noreply.github.com>
2025-09-06 16:44:13 -0300

    Criação de forma de pagamento no js

--- js/carrinho/carrinho.js
+++ js/carrinho/carrinho.js
@@ -42,7 +42,7 @@ function fecharModal() {
 
 //Passando carrinho
 
-function formaPagamento(){
+function paginaFormaPagamento(){
     const previsao = document.querySelector('.box-endereco .previsao');
     const previsaoEditar = document.querySelector('.box-endereco .previsao-editar');
     const boxPratos = document.querySelector('.box-pratos');
@@ -65,7 +65,72 @@ function formaPagamento(){
     <p>R$77.43</p>
     </div>`;
 
-    boxSubTotal.innerHTML = '';
+    boxSubTotal.innerHTML = `<hr>
+
+                <div class="radio-buttons">
+                    <label class="radio-btn">
+                        <input type="radio" name="opcao" value="1" checked>
+                        <p>Cartão <i class="ph ph-credit-card"></i></p>
+                    </label>
+                    <label class="radio-btn">
+                        <input type="radio" name="opcao" value="1">
+                        <p>Dinheiro <i class="ph ph-credit-card"></i></p>
+                    </label>
+                    <label class="radio-btn">
+                        <input type="radio" name="opcao" value="1">
+                        <p>Pix <i class="ph ph-credit-card"></i></p>
+                    </label>
+                </div>
+                <div class="forma-pagamento">
+                    <!--
+                    <input type="number" name="" id="" placeholder="Exemplo: R$24,00">
+                    <div class="d-flex"><i class="ph ph-warning-circle alerta-cor"></i><p class="ms-2">O QR Code Pix vai ser gerado na hora da entrega</p></div>
+                    -->
+                    
+                    <div class="d-flex align-items-center">
+                        <input type="radio" name="cartao-forma" value="credito" id="credito">
+                        <label for="credito">Credito</label>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <input type="radio" name="cartao-forma" value="debito" id="debito">
+                        <label for="debito">Debito</label>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <input type="radio" name="cartao-forma" value="vale" id="vale">
+                        <label for="vale">Vale-Alimentação</label>
+                    </div>
+                    
+                </div>`;
     
 
+}
+
+const formaPagamento = document.querySelector('.radio-buttons input[name="opcao"]:checked').value;
+if(formaPagamento){
+    const boxFormaPagamento = document.querySelector('.forma-pagamento');
+    if(formaPagamento === 'credito'){
+        boxFormaPagamento.innerHTML = `
+            <div class="d-flex align-items-center">
+                <input type="radio" name="cartao-forma" value="credito" id="credito">
+                <label for="credito">Credito</label>
+            </div>
+            <div class="d-flex align-items-center">
+                <input type="radio" name="cartao-forma" value="debito" id="debito">
+                <label for="debito">Debito</label>
+            </div>
+            <div class="d-flex align-items-center">
+                <input type="radio" name="cartao-forma" value="vale" id="vale">
+                <label for="vale">Vale-Alimentação</label>
+            </div>
+        `;
+    }
+    else if(formaPagamento === 'dinheiro'){
+        boxFormaPagamento.innerHTML = `
+            <input type="number" name="" id="" placeholder="Exemplo: R$24,00">
+        `;
+    }else if(formaPagamento === 'pix'){
+        boxFormaPagamento.innerHTML = `
+            <div class="d-flex"><i class="ph ph-warning-circle alerta-cor"></i><p class="ms-2">O QR Code Pix vai ser gerado na hora da entrega</p></div>
+        `;
+    }
 }
