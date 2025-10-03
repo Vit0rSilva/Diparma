c74a910639fe416da922d545560bb69268b6c1d7
Vitor Silva <188924537+Vit0rSilva@users.noreply.github.com>
2025-10-02 22:39:09 -0300

    Adcionando fluxo de etapas em pagamento/conclusão

--- Diparma/src/app/componetes/carrinho/carrinho.ts
+++ Diparma/src/app/componetes/carrinho/carrinho.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {CarrinhoService} from './service/carrinho';
 import { Api } from '../../core/service/api';
 import { Endereco } from '../../models/endereco.model';
@@ -6,6 +6,7 @@ import { Endereco } from '../../models/endereco.model';
 @Component({
   selector: 'app-carrinho',
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './carrinho.html',
   styleUrl: './carrinho.scss'
 })
@@ -13,6 +14,8 @@ export class Carrinho {
   carrinhoAberto: boolean = false;
   enderecos: Endereco[] = [];
   enderecoTodosNullPrincipal: boolean = false;
+  etapaCesta: 'cesta' | 'pagamento' | 'confirmacao' = 'cesta';
+  etapaPagamento: 'cartao' | 'pix' | 'dinheiro' = 'cartao';
 
   constructor ( private carrinhoService: CarrinhoService, private api: Api) { }
 
@@ -46,10 +49,60 @@ export class Carrinho {
     });
 
   }
-
-
-
   fecharCarrinho() {
     this.carrinhoService.fecharCarrinho();
   }
+
+  //-------------Fluxo de etapas do carrinho
+
+  //Fluxo de etapa pagamento
+  irParaPagamento(){
+    this.etapaCesta = 'pagamento';
+  }
+
+  caixaCupom: 'aberto' | 'fechado' = 'fechado';
+  cupomDesconto(opcao: string){
+    if(opcao === 'fechado'){
+      this.caixaCupom = 'fechado';
+    }else{
+      this.caixaCupom = 'aberto';
+    }
+  }
+
+  //Fluxo de etapa confirmação
+  tempo:number = 0;
+  private intervalo: any;
+  
+  confirmarPedido(){
+    this.tempo = 10;
+    this.etapaCesta = 'confirmacao';
+
+    this.intervalo = setInterval(() => {
+      if (this.tempo > 0) {
+        this.tempo--;
+      } else {
+        clearInterval(this.intervalo);
+        console.log('Tempo acabou!');
+        this.etapaCesta = 'cesta';
+      }
+    }, 1000);
+  }
+
+  voltarParaCesta(){
+    this.etapaCesta = 'cesta';
+
+    this.etapaPagamento = 'cartao';
+  }
+
+  //Fluxo de etapa pagamento
+  selecionarCartao(){
+    this.etapaPagamento = 'cartao';
+  }
+  selecionarPix(){
+    this.etapaPagamento = 'pix';
+  }
+  selecionarDinheiro(){
+    this.etapaPagamento = 'dinheiro';
+  }
+
 }
